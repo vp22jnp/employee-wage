6eaafc81d8ee7c845371971f40b3b9c9bc05f96b
vp22j <vp22jnp@gmail.com>
2022-09-23 23:33:35 +0530

    initial commit

--- snakeAndLadderUc1.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabday4;
-
-public class snakeAndLadderUc1 {
-    public static void main(String[] args) {
-        int position=0;
-        System.out.println("position is:" +position);
-    }
-}
--- snakeAndLadderuc2.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabday4;
-import java.util.Random;
-public class snakeAndLadderuc2 {
-    public static void main(String[] args) {
-        Random random=new Random();
-        int dice= random.nextInt(6)+1;
-        System.out.println("number is::" +dice);
-    }
-}
